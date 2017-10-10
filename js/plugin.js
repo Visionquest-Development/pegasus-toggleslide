09a4954c2b341cf84aa8283de59dafd4883073aa
Jim O'Brien <jim.obrien3@gmail.com>
2017-10-09 23:29:23 -0400

    third commit

--- js/plugin.js
+++ js/plugin.js
@@ -3,7 +3,7 @@
 
     $(".pegasus-trigger").on("click", function(event){
 
-        console.log( this );
+        //console.log( this );
 
         //first open the container with the social stuff
         $( this ).parents('.pegasus-toggle').find('.toggle-content').slideToggle();
